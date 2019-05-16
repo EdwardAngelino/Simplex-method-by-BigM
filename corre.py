1f9ad0d630238d0bce3d1fd3aa6cfc4a45e84030
Edward Angelino <49659542+EdwardAngelino@users.noreply.github.com>
2019-05-16 18:17:18 -0500

    simplifica la corrida

--- corre.py
+++ corre.py
@@ -8,14 +8,21 @@ b=[11,3,1]
 ine=[1,-1,0]
 prob ='min' '''
 
-A=[[1,1,0],
+'''A=[[1,1,0],
     [1,0,1],
     [0,1,1]]
 c=[1,-1,3] 
 b=[20,5,10]
 ine=[1,0,-1] # 1:'<=', -1:'>=', 0:'='
-prob='max'
+prob='max' '''
+
+
+A=[[1,1],[1,0],[0,1],[1,-2]]
+b=[21,13,6,0]
+c=[0.10,0.08]
+ine=[1,1,-1,0]
+prob='min'
 
 corre_simplex(A,b,c,ine,prob)
-res_ecuaciones(A,b)
-inv_matriz(A)
+#res_ecuaciones(A,b)
+#inv_matriz(A)
