56b50f6170565875bce38a7ed5c8abb7b260f765
Edward Angelino <49659542+EdwardAngelino@users.noreply.github.com>
2019-05-20 10:19:07 -0500

    Update corre.py

--- corre.py
+++ corre.py
@@ -9,7 +9,7 @@ print('\nEjemplo 1\n---------')
 c = [ 2,  3 ]
 A = [[1 ,4], [2, 3],[ 2, 1]]
 b = [4, 6 ,4]
-inq = [1, 1, 1]
+inq = [-1, -1, -1]
 corre_simplex(A,b,c,inq,'max')
 
 
@@ -17,7 +17,7 @@ print('\nEjemplo 2\n---------')
 c=[-3,1,1]
 b=[11,3,1]
 A=[[1, -2, 1],[-4, 1, 2],[-2, 0, 1]]
-inq=[1, -1, 0]
+inq=[-1, 1, 0]
 corre_simplex(A,b,c,inq,'min') 
 
 print('\nEjemplo 3\n---------')
@@ -31,21 +31,21 @@ print('\nEjemplo 4\n---------') #aqui se puede observar que s1 <= -M, es infacti
 c = [1,2]
 A = [[1, 1] , [3, 2]]
 b = [4, 6]
-inq = [-1, 1]
+inq = [1, -1]
 corre_simplex(A,b,c,inq,'max') 
 
 print('\nEjemplo 5\n---------')  #sin limites s1 es positivo todavia  
 c = [-1, -2]
 A = [[1, 1], [1, -1]]
 b = [3,1]
-inq = [-1,-1]
+inq = [1,1]
 corre_simplex(A,b,c,inq,'min') 
 
 print('\nEjemplo 6\n---------') # s1,s2,s3 <= -M,es infactible
 c = [ 1, 1, 1, 1 ]
 A = [ [1, 2, 0 ,1],[0, 0, 1, 3],[ 1, 0, 1, 0] ]
 b = [2, 3, 6 ]
-inq = [ 1, 1, -1 ]
+inq = [ -1, -1, 1 ]
 corre_simplex(A,b,c,inq,'max') 
 
 
@@ -53,6 +53,13 @@ print('\nEjemplo 7\n---------')  #sin limites s3 > 0
 c = [ 1, 2, 3, 4 ]
 A = [ [1, 2, -1, 2],[ 2, 0, 3, -1] ]
 b = [ 4, 6 ]
-inq = [ -1, 0 ]
+inq = [ 1, 0 ]
 corre_simplex(A,b,c,inq,'max') 
+
+print('\nEjemplo 8\n---------')  #sin limites s3 > 0  
+c = [-1,2,-3]
+A = [ [1,1,1],[-1,1,2],[ 0, 2, 3],[0,0,1] ]
+b = [ 6,4,10,2 ]
+inq = [ 0,0,0,-1]
+corre_simplex(A,b,c,inq,'min') 
 
